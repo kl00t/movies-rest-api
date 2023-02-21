7d2cc8dd616d0c9a8be841624bfa33f8652958ec
Scott Vaughan <scott.vaughan@ensono.com>
2023-02-21 11:12:30 +0000

    Implementing endpoints

---
+++ Movies.Api/Controllers/MoviesController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Api.Mapping;
+using Movies.Application.Models;
+using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
+using Movies.Contracts.Responses;
+
+namespace Movies.Api.Controllers;
+
+[ApiController]
+public class MoviesController : ControllerBase
+{
+	private readonly IMovieRepository _movieRepository;
+
+	public MoviesController(IMovieRepository movieRepository)
+	{
+		_movieRepository = movieRepository;
+	}
+
+	[HttpPost(ApiEndpoints.Movies.Create)]
+	public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
+	{
+		var movie = request.MapToMovie();
+		await _movieRepository.CreateAsync(movie);
+		return Created($"/{ApiEndpoints.Movies.Create}/{movie.Id}", movie);
+	}
+
+	[HttpGet(ApiEndpoints.Movies.Get)]
+	public async Task<IActionResult> Get([FromRoute] Guid id)
+	{
+		var movie = await _movieRepository.GetByIdAsync(id);
+		if (movie is null)
+		{
+			return NotFound();
+		}
+
+		var movieResponse = movie.MapToResponse();
+		return Ok(movieResponse);
+	}
+
+	[HttpGet(ApiEndpoints.Movies.GetAll)]
+	public async Task<IActionResult> GetAll()
+	{
+		var movies = await _movieRepository.GetAllAsync();
+        var moviesResponse = movies.MapToResponse();
+        return Ok(moviesResponse);
+    }
+}
