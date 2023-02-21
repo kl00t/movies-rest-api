433f948ac399a3d54b2b2934f39c57f7cb5d028a
Scott Vaughan <scott.vaughan@ensono.com>
2023-02-21 15:55:10 +0000

    Implementing validation

--- Movies.Api/Program.cs
+++ Movies.Api/Program.cs
@@ -1,3 +1,4 @@
+using Movies.Api.Mapping;
 using Movies.Application;
 using Movies.Application.Database;
 
@@ -22,6 +23,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseMiddleware<ValidationMappingMiddleware>();
 
 app.MapControllers();
 
