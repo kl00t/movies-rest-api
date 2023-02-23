b575446c17a6264863c4735d9f856f0538756b50
Scott Vaughan <scott.vaughan@ensono.com>
2023-02-23 09:21:23 +0000

    Fix CA warnings.

--- Identity.Api/TokenGenerationRequest.cs
+++ Identity.Api/TokenGenerationRequest.cs
@@ -3,8 +3,8 @@
 public class TokenGenerationRequest
 {
     public Guid UserId { get; set; }
-    
-    public string Email { get; set; }
+
+    public string Email { get; init; } = null!;
 
     public Dictionary<string, object> CustomClaims { get; set; } = new();
 }
