bc073b498b0f3e7ff017c67d5a09c6ae035b916e
Scott Vaughan <3264879+kl00t@users.noreply.github.com>
2023-02-24 13:14:03 +0000

    Advanced versioning (#2)
    Co-authored-by: Scott Vaughan <scott.vaughan@ensono.com>

--- Movies.Api/Program.cs
+++ Movies.Api/Program.cs
@@ -1,3 +1,4 @@
+using Asp.Versioning;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Movies.Api.Auth;
@@ -40,6 +41,13 @@ builder.Services.AddAuthorization(x =>
             c.User.HasClaim(m => m is { Type: AuthConstants.TrustedMemberClaimName, Value: "true" })));
 });
 
+builder.Services.AddApiVersioning(x =>
+{
+    x.DefaultApiVersion = new ApiVersion(1.0);
+    x.AssumeDefaultVersionWhenUnspecified = true;
+    x.ReportApiVersions = true;
+    x.ApiVersionReader = new MediaTypeApiVersionReader("api-version");
+}).AddMvc();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
